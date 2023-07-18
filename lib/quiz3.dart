a3c8fb18f01289fc4dfaf412063347db534caf71
Aayush Paigwar <aayushpaigwar@Aayushs-Air.bbrouter>
2023-07-19 01:42:17 +0530

    UI Modified

--- lib/quiz3.dart
+++ lib/quiz3.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_algo/congrats.dart';
-import 'package:go_algo/login3.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'dart:async';
 
